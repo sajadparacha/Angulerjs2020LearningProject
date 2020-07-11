15dac6aa87e16b1ef10560505f91d996bb230969
Sajjad Ahmed Paracha <sajadparacha@gmail.com>
2020-07-11 23:16:20 +0300

    Added first user component
    Added first user component

---
+++ myfirstangularapp2020/src/app/user.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+@Component({
+    selector:'app-user',
+    template:`
+    <p>Hello Sajjad Yuour user component is displayed here</p>
+    `
+})
+export class UserComponent{}
