07f7aa9ff6dba28260d00e91650579048619566f
Sajjad Ahmed Paracha <sajadparacha@gmail.com>
2020-07-12 17:25:16 +0300

    Update user.component.ts

--- myfirstangularapp2020/src/app/user.component.ts
+++ myfirstangularapp2020/src/app/user.component.ts
@@ -2,10 +2,20 @@ import {Component} from '@angular/core'
 @Component({
     selector:'app-user',
     template:`
+    <!--Below we are binding the input event with and event handler function define in our ts -->
+    <input type="text" (input)="onUserInput($event)" [value]="name"/>
     <p>Hello {{name}} Your user component is displayed here</p>
     `
 })
 export class UserComponent{
 
-    name='Sajjad Ahmed Paracha'
+    name='Sajjad Ahmed';
+    /**
+     * 
+     * @param event 
+     * The below method will be called whenever something is entered in the above input.
+     */
+    onUserInput(event){
+        this.name=event.target.value;
+    }
 }
